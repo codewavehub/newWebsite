114c14aa94d08eb596e9907e931740817c069bf1
Suhaib <suhaib142003@gmail.com>
2024-07-14 23:12:49 +0530

    logo scroll

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,19 @@ module.exports = {
         'primary': '#0097B2',
         'primary-light': '#00c1e3',
         'primary-dark': '#00798f'
-      }
+      },
+      fontFamily: {
+        'poppins': ['Poppins','sans-serif']
+      },
+      animation: {
+        'infinite-scroll': 'infinite-scroll 25s linear infinite',
+      },
+      keyframes: {
+        'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
+      }        
     },
   },
   plugins: [],
