1180b3012a109d5ddc3c8a35e0098dc2571f5567
Suhaib14A <suhaib_03@outlook.com>
2024-10-12 04:24:04 +0530

    new sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,7 @@ module.exports = {
           from: { transform: 'translateX(0)' },
           to: { transform: 'translateX(-100%)' },
         }
-      }        
+      },        
     },
   },
   plugins: [],
